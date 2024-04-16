13c6deeb1408ff7fb887d0faaeaba7294a12b0f4
gaozhiheng <gaozhiheng@kongfz.com>
2024-04-16 19:39:21 +0800

    rm welcome.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-// import { welcome } from './welcome';
 import registerHover from './registerHover';
 import registerDefinition from './registerDefinition';
 import { getMixinsPaths } from './getMixins';
